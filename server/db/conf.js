958bdbf2a752a513a83e4938953e4abcb69dd120
kir3kin <kir3kin@gmail.com>
2021-11-22 10:23:16 +0100

    add new db

--- server/db/conf.js
+++ server/db/conf.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnect = async () => {
 	try {
-		await mongoose.connect('mongodb+srv://restApi:TYxXBv8awNU9D9q@experiments.p39am.mongodb.net/contacts', {
+		await mongoose.connect('mongodb+srv://restAPI_admin:lucifierM0rn1ninSt2r@cluster0.iupx5.mongodb.net/contacts', {
 			useNewUrlParser: true,
 			useFindAndModify: false,
 			useUnifiedTopology: true,
